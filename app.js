a1e85d751c66d208bf087de8cefee2deec807148
Allam GowriSankar <allamgowrisankar001@gmail.com>
2025-05-17 13:40:13 +0530

    cors for video upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: 'https://lms.trizenventures.com' || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
--- server.js
+++ server.js
@@ -42,8 +42,10 @@ const app = express();
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5000mb' }));
+app.use(express.urlencoded({ limit: '50000mb', extended: true }));
 
 // app.options('*', cors()); // handle preflight
 
