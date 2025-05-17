b8b7a8f9deea28ea2e61ca8aed3eaf04a599c283
divyamaddipati <21b01a12a2@svecw.edu.in>
2025-05-17 12:46:43 +0530

    cors

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Enable CORS for image requests
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'origin');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
