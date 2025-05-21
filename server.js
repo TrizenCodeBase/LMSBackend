1698bfa5fa0d37dd663c6f7cfc2341b53155840f
Allam GowriSankar <allamgowrisankar001@gmail.com>
2025-05-21 19:26:26 +0530

    LMSBackend

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Enable CORS for image requests
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://lms.trizenventures.com/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
